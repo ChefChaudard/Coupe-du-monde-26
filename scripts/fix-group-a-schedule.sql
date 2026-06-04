e6921b7c3e3ca782e8fab6e6865613debb789d90
Fabrice Beral <fabrice.beral@alenia.io>
2026-06-04 23:29:56 +0200

    Fix Group A schedule

---
+++ scripts/fix-group-a-schedule.sql
@@ -0,0 +1,35 @@
+update public.matches
+set team_a = $$Mexique$$,
+    team_b = $$Afrique du Sud$$,
+    kickoff_at = $$2026-06-11T19:00:00+00:00$$
+where id = 1;
+
+update public.matches
+set team_a = $$République de Corée$$,
+    team_b = $$Tchéquie$$,
+    kickoff_at = $$2026-06-12T02:00:00+00:00$$
+where id = 2;
+
+update public.matches
+set team_a = $$Tchéquie$$,
+    team_b = $$Afrique du Sud$$,
+    kickoff_at = $$2026-06-18T16:00:00+00:00$$
+where id = 3;
+
+update public.matches
+set team_a = $$Mexique$$,
+    team_b = $$République de Corée$$,
+    kickoff_at = $$2026-06-19T01:00:00+00:00$$
+where id = 4;
+
+update public.matches
+set team_a = $$Afrique du Sud$$,
+    team_b = $$République de Corée$$,
+    kickoff_at = $$2026-06-25T01:00:00+00:00$$
+where id = 5;
+
+update public.matches
+set team_a = $$Tchéquie$$,
+    team_b = $$Mexique$$,
+    kickoff_at = $$2026-06-25T01:00:00+00:00$$
+where id = 6;
